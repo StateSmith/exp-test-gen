6d773ce0bcca3c9ec7b8d3eaad856435ed54879b
Adam Fraser-Kruck <adamfk@gmail.com>
2024-05-14 17:43:34 -0500

    auto generated tests idea

--- src/LightSm.jest.test.js
+++ src/LightSm.jest.test.js
@@ -12,41 +12,76 @@ globalThis.light_blue = jest.fn();
 globalThis.light_yellow = jest.fn();
 globalThis.light_red = jest.fn();
 
+
+////////////////////////////////////////////////////////////////////////////////
+// START AUTO GENERATED HELPERS
+// ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Sets up the state machine for testing.
+ * AUTO GENERATED
+ * @param {LightSm} sm 
+ */
+function start(sm) {
+    sm.start();
+    expect(sm.stateId).toBe(LightSm.StateId.OFF);
+}
+
+/**
+ * Takes the state machine from OFF to ON1
+ * AUTO GENERATED
+ * @param {LightSm} sm 
+ */
+function start_to_on1(sm) {
+    start(sm);
+    sm.dispatchEvent(LightSm.EventId.INCREASE);
+    expect(sm.stateId).toBe(LightSm.StateId.ON1);
+}
+
+/**
+ * Takes the state machine from OFF to ON2
+ * AUTO GENERATED
+ * @param {LightSm} sm 
+ */
+function start_to_on2(sm) {
+    start_to_on1(sm);
+    sm.dispatchEvent(LightSm.EventId.INCREASE);
+    expect(sm.stateId).toBe(LightSm.StateId.ON2);
+}
+////////////////////////////////////////////////////////////////////////////////
+// ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+// END AUTO GENERATED HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+
+// start of user code
+
 beforeEach(() => {
     jest.clearAllMocks();
 });
 
 test('starts in the off state', () => {
     const sm = new LightSm();
-    sm.start();
-    expect(sm.stateId).toBe(LightSm.StateId.OFF);
+    start(sm);
 });
 
 test('println to be called once on startup', () => {
     const sm = new LightSm();
-    sm.start();
+    start(sm);
     expect(globalThis.println.mock.calls).toHaveLength(1);
 });
 
 test('light is blue when turned on', () => {
-    // Arrange
     const sm = new LightSm();
-    sm.start();
-
-    // Act
-    sm.dispatchEvent(LightSm.EventId.INCREASE);
-
-    // Assert
-    expect(sm.stateId).toBe(LightSm.StateId.ON1);
+    start_to_on1(sm);
     expect(globalThis.light_blue.mock.calls).toHaveLength(1);
 });
 
 test('light can be turned off', () => {
     // Arrange
     const sm = new LightSm();
-    sm.start();
-    sm.dispatchEvent(LightSm.EventId.INCREASE);
-    expect(sm.stateId).toBe(LightSm.StateId.ON1);
+    start_to_on1(sm);
 
     // Act
     sm.dispatchEvent(LightSm.EventId.DIM);
@@ -58,17 +93,9 @@ test('light can be turned off', () => {
 test('count how many times INCREASE is pressed in ON2', ()=>{
     // Arrange
     const sm = new LightSm();
-    sm.start();
-    sm.dispatchEvent(LightSm.EventId.INCREASE);
-    sm.dispatchEvent(LightSm.EventId.INCREASE);
-    expect(sm.stateId).toBe(LightSm.StateId.ON2);
+    start_to_on2(sm);
     expect(sm.vars.count).toBe(0);
-
-    // Act
     sm.dispatchEvent(LightSm.EventId.INCREASE);
-
-    // Assert
-    expect(sm.stateId).toBe(LightSm.StateId.ON2);
     expect(sm.vars.count).toBe(1);
 })
 
