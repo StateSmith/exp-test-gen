8d4661d50a17f2e203f825e62f722af1c02c8c41
Mike Burton <mb-github@niskala.org>
2024-05-19 09:26:56 -0700

    add mock definitions

--- src/LightSm.mocks.test.h
+++ src/LightSm.mocks.test.h
@@ -1,4 +1,4 @@
-void println(char const * str);
-void light_red();
-void light_blue();
-void light_yellow();
+void println(char const * str) {}
+void light_red() {}
+void light_blue() {}
+void light_yellow() {}
